386bd8bd07896858290074c151d7c5bc31d273a8
sphinx-austin <austinjuniour4@gmail.com>
2023-04-25 09:41:33 +0300

    include () to print prediccio

--- demo.py
+++ demo.py
@@ -1,13 +1,13 @@
 '''
-Created on Mar 24, 2013
+Created on Mar 24, 2023
 
-@author: ctrl-alt-d
+@author: sphinx-austin
 '''
 
 from lightpredictor import utils
 
 if __name__ == '__main__':
-    
+
     values = {}
     values['nom_nivell'] = 'ESO'
     values['hora_inici'] = '09:15:00'
@@ -15,9 +15,9 @@ if __name__ == '__main__':
     values['assistenciaMateixaHora2WeekBefore'] = 'NA'
     values['assistenciaMateixaHora3WeekBefore'] = 'NA'
     values['assistenciaaHoraAnterior'] = 'Present'
-    
+
     prediccio = utils.predict('pmml-models/dt.pmml', values)
-    print prediccio
+    print(prediccio)
 
     values = {}
     values['nom_nivell'] = 'CFGM Infor'
@@ -26,9 +26,9 @@ if __name__ == '__main__':
     values['assistenciaMateixaHora2WeekBefore'] = 'Absent'
     values['assistenciaMateixaHora3WeekBefore'] = 'Absent'
     values['assistenciaaHoraAnterior'] = 'Absent'
-        
+
     prediccio = utils.predict('pmml-models/dt.pmml', values)
-    print prediccio
+    print(prediccio)
 
     values = {}
     values['nom_nivell'] = 'ESO'
@@ -37,9 +37,6 @@ if __name__ == '__main__':
     values['assistenciaMateixaHora2WeekBefore'] = 'NA'
     values['assistenciaMateixaHora3WeekBefore'] = 'NA'
     values['assistenciaaHoraAnterior'] = 'NA'
-        
+
     prediccio = utils.predict('pmml-models/dt.pmml', values)
-    print prediccio
-
-
-    
+    print(prediccio)
